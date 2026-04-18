37c157eb00f03fc5917629aeacf650c659169a9b
finnbennett <finlaybennett@gmail.com>
2026-04-17 19:57:09 -0700

    fix: update projectId handling to include fallback for development environment

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,7 +2,11 @@ function optionalValue(v: string | undefined): string {
   return (v ?? '').trim();
 }
 
-export const projectId = optionalValue(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
+const linkedProjectId = '3dl9wh1l';
+const fallbackProjectId = process.env.NODE_ENV === 'production' ? linkedProjectId : '';
+
+export const projectId =
+  optionalValue(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) || fallbackProjectId;
 export const dataset = optionalValue(process.env.NEXT_PUBLIC_SANITY_DATASET) || 'production';
 export const apiVersion =
   optionalValue(process.env.NEXT_PUBLIC_SANITY_API_VERSION) || '2026-03-01';
